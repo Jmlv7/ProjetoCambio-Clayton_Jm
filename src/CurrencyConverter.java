3b1e2387822f852a66a8ea647928bd9168ca7700
Jmlv7 <165213783+Jmlv7@users.noreply.github.com>
2025-02-10 19:16:55 -0300

    Update CurrencyConverter.java

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class CurrencyConverter {
+    public static final double IOF = 0.06;
+
+    public static double converter(double cotacao, double quantidadeDolares) {
+        return quantidadeDolares * cotacao * (1 + IOF);
+    }
 }
