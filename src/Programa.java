1e805c236b9f7615a2fb3c561eea0e855df32066
Jmlv7 <165213783+Jmlv7@users.noreply.github.com>
2025-02-10 19:16:32 -0300

    Update Programa.java

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Programa {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Qual o valor do dólar?");
+        double cotacao = sc.nextDouble();
+        System.out.println("Quantos dólares serão comprados?");
+        double quantidadeDolares = sc.nextDouble();
+
+        double valorEmReais = CurrencyConverter.converter(cotacao, quantidadeDolares);
+
+        System.out.printf("Valor a ser pago em reais = %.2f%n", valorEmReais);
+
+        sc.close();
+    }
 }
